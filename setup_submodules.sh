09dcd1eb7589cd2ecf68d1e14bf718eee1b76d78
taichi <taichi730@gmail.com>
2019-09-19 14:21:58 +0900

    changed way to import submodules

--- setup_submodules.sh
+++ setup_submodules.sh
@@ -1,8 +1,16 @@
 #! /bin/bash -f
 submodules=(
-  tue
+  "https://github.com/taichi-ishitani/tue.git tue c4ce84ebd2cb52c7fb97f0c32101f53141d85132"
 )
 
-for submodule in ${submodules[@]} ; do
-  git submodule update --init ${submodule}
+for ((i=0; $i < ${#submodules[*]}; i++)) do
+  temp=(${submodules[$i]})
+  url=${temp[0]}
+  path=${temp[1]}
+  hash=${temp[2]}
+  if [ ! -d $path ]; then
+    git clone $url $path
+  fi
+  $(cd $path; git checkout $hash)
 done
+
