4a511061abc7829aef3d278e6ba70dd7d4b70afa
taichi <taichi730@gmail.com>
2020-07-01 11:37:47 +0900

    added tvip-common to setup script

--- setup_submodules.sh
+++ setup_submodules.sh
@@ -1,6 +1,7 @@
 #! /bin/bash -f
 submodules=(
   "https://github.com/taichi-ishitani/tue.git tue ada80d6344542549070e26db5083a153c5cbaade"
+  "https://github.com/taichi-ishitani/tvip-common.git tvip-common 1b41f9c1adc3724e6430c795e9fa4c0b5e8c1af0"
 )
 
 for ((i=0; $i < ${#submodules[*]}; i++)) do
