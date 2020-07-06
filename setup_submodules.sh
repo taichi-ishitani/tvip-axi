09328aa8495d4317c14c6fd7cfefc821adafd17e
Taichi Ishitani <taichi730@gmail.com>
2020-07-06 10:44:20 +0900

    updated tue submodule

--- setup_submodules.sh
+++ setup_submodules.sh
@@ -1,6 +1,6 @@
 #! /bin/bash -f
 submodules=(
-  "https://github.com/taichi-ishitani/tue.git tue 329828f379715eda165404974e6de3c7403ed819"
+  "https://github.com/taichi-ishitani/tue.git tue ecc2f0a7562294676f3f7510ce58eba18b2bbcb0"
   "https://github.com/taichi-ishitani/tvip-common.git tvip-common 033183e63e1a5f325f43906972e78d18ebced220"
 )
 
@@ -12,6 +12,6 @@ for ((i=0; $i < ${#submodules[*]}; i++)) do
   if [ ! -d $path ]; then
     git clone $url $path
   fi
-  $(cd $path; git checkout $hash)
+  $(cd $path; git fetch; git checkout $hash)
 done
 
