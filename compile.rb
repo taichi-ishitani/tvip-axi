f77e027df516488150f13615f93c4aa78a3806e5
Taichi Ishitani <taichi730@gmail.com>
2022-12-17 23:53:25 +0900

    add flgen filelist

---
+++ compile.rb
@@ -0,0 +1,8 @@
+if ENV.key?('TVIP_COMMON_HOME')
+  file_list File.join(ENV['TVIP_COMMON_HOME'], 'compile.f')
+elsif Dir.exist?(File.join(__dir__, 'tvip-common'))
+  file_list 'tvip-common/compile.rb', from: :current
+end
+
+include_directory 'src'
+source_file 'src/tvip_axi_pkg.sv'
