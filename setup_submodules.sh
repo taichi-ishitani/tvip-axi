cd53d9275610438a1960e967b4b896c816d61ce6
ishitani <ishitani@pezy.co.jp>
2024-06-28 16:49:59 +0900

    update tue

--- setup_submodules.sh
+++ setup_submodules.sh
@@ -1,6 +1,6 @@
 #! /bin/bash -f
 submodules=(
-  "https://github.com/taichi-ishitani/tue.git tue d42734b16c613521655f29edc11294251e8b5b39"
+  "https://github.com/taichi-ishitani/tue.git tue b49856a47ed162fd0eea4509f2495465d0091e7f"
   "https://github.com/taichi-ishitani/tvip-common.git tvip-common be09036727aac5be4c483536b12e7a1e3c0c8b8b"
 )
 
